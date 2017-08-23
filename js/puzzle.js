1c0474b3ebf521eb9811ad4cefc831871829a88f
William Robinson <wamblone@wamblone-2.local>
2017-08-22 19:24:20 -0700

    Example

---
+++ js/puzzle.js
@@ -0,0 +1,15 @@
+$('document').ready(function() {
+
+  $('img.puzzle-image').click(function(){
+
+    var element = $(this);
+
+    var src = element.attr('src');
+    var alt_src = element.data('alt-src');
+
+    element.attr('src',alt_src);
+    element.data('alt-src',src);
+
+  });
+
+});
