d48bd5bb4129e4c44b0e07f5c84476a486cd4017
McCabe <mccabe@wambl.com>
2017-08-22 22:11:13 -0700

    updated working

--- jQuery.js
+++ jQuery.js
@@ -1,18 +1,14 @@
 $('document').ready(function() {
-  $('p1').click(function() {
-    alert('hello');
+  $('img.puzzle-image').click(function() {
+    var element = $(this);
+    var src = element.attr('src');
+    var alt_src = element.data('alt-src');
+
+    element.attr('src', alt_src);
+    element.data('alt-src', src);
   })
 
 });
-$('img').click(function() {
-  console.log('data-alt-src value is', $(this).attr('data-alt-src'));
-});
-
-$('img').click(function() {
-  $(this).hide();
-  $('img[src *= "ninja"]').show();
-});
-
 /*
 $("#infoToggler img").click(function() {
   $("#infoToggler img").toggle(); //you need to toggle images both
